a8925e01e78add9f1cce9cdb8309664154bc5154
Jrivera2006 <150264252+Jrivera2006@users.noreply.github.com>
2024-02-09 10:50:46 -0500

    Add files via upload

---
+++ assignments/circle.py
@@ -0,0 +1,9 @@
+import math
+radius = int(input("Enter the radius of the cirle: "))
+print(int(radius))
+
+cirmcumference = 2 * math.pi * radius
+
+area = math.pi * radius ** 2
+
+print(f'The area is {area}, the cirmcumference is {cirmcumference}, for radius {radius}')
