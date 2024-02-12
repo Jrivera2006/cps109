dfa817d0c58fdb3f4ecc1d61e45d1d16246c8e52
Jrivera2006 <150264252+Jrivera2006@users.noreply.github.com>
2024-02-12 10:21:40 -0500

    Update circle.py

--- assignments/circle.py
+++ assignments/circle.py
@@ -1,3 +1,16 @@
+"""
+
+Create a file named circle.py in all lowercase.
+Add a docstring to the top of the file containing a description and major tasks of the program (see details below).
+Import the math module.
+Prompt the user to enter the radius of the circle.
+Convert the user entered value from a string to an integer.
+Calculate the circumference of the circle and save as a variable named circumference.
+Calculate the area of the circle and save as a variable named area.
+Use the print function to display the circumference, area and radius. Use the Python Formatted string literals (also called f-strings for short).
+Upload your file to your GitHub account and send me the link.
+"""
+
 import math
 radius = int(input("Enter the radius of the cirle: "))
 print(int(radius))
