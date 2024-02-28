79eec630c241adc16b5ff8540da9413244230539
Jrivera2006 <150264252+Jrivera2006@users.noreply.github.com>
2024-02-28 09:32:50 -0500

    Add files via upload

---
+++ assignments/grades.py
@@ -0,0 +1,38 @@
+grade = int(input('Enter your grade in number form: '))
+
+
+if 93 <= grade and grade <= 100:
+    print(f'if your grade is {grade} is a A.')
+
+if 90 <= grade and grade <= 92:
+    print(f'if your grade is {grade} is a A-.')
+
+if 87 <= grade and grade <= 89:
+    print(f'if your grade is {grade} is a B+.')
+
+if 83 <= grade and grade <= 86:
+    print(f'if your grade is {grade} is a B.')
+
+if 80 <= grade and grade <= 82:
+    print(f'if your grade is {grade} is a B-.')
+
+if 77 <= grade and grade <= 79:
+    print(f'if your grade is {grade} is a C+.')
+
+if 73 <= grade and grade <= 76:
+    print(f'if your grade is {grade} is a C.')
+
+if 70 <= grade and grade <= 72:
+    print(f'if your grade is {grade} is a C-.')
+
+if 67 <= grade and grade <= 69:
+    print(f'if your grade is {grade} is a D+.')
+
+if 63 <= grade and grade <= 66:
+    print(f'if your grade is {grade} is a D.')
+
+if 60 <= grade and grade <= 62:
+    print(f'if your grade is {grade} is a D-.')
+
+if grade < 60:
+    print(f'if your grade is {grade} is a F.')
